aaccfc117729dd8c4a962ab10080ab6dad644520
Narsis93 <106112306+Narsis93@users.noreply.github.com>
2025-11-27 01:46:36 +0300

    Update login.cy.js

--- Automation/cypress/e2e/login.cy.js
+++ Automation/cypress/e2e/login.cy.js
@@ -1,1 +1,9 @@
-
+describe('Login Test', () => {
+  it('User should login successfully', () => {
+    cy.visit('/');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+    cy.url().should('include', '/inventory');
+  });
+});
