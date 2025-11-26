3f16d95ddd73638e6c5a3eb5554bdf6c0ce4d0bf
Narsis93 <106112306+Narsis93@users.noreply.github.com>
2025-11-27 01:47:06 +0300

    Update add_to_cart.cy.js

--- Automation/cypress/e2e/add_to_cart.cy.js
+++ Automation/cypress/e2e/add_to_cart.cy.js
@@ -1,1 +1,10 @@
-
+describe('Add To Cart', () => {
+  it('should add item to cart', () => {
+    cy.visit('/');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+    cy.contains('Add to cart').first().click();
+    cy.get('.shopping_cart_badge').should('contain', '1');
+  });
+});
